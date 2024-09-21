12388866769131b5534343a88df16afc0e96d422
habibullah47 <habib.world.121@gmail.com>
2024-09-21 16:07:50 +0600

    await & async function

--- scratch.dart
+++ scratch.dart
@@ -2,6 +2,7 @@ void main() {
   allTaskCompleted();
 }
 
+//Using async here for completing result
 void allTaskCompleted() async {
   task1();
   String taskTwoResult = await task2();
@@ -12,6 +13,7 @@ void task1() {
   print('Task 1 completed');
 }
 
+///[Future] as return type for async function. Example as coffee receipt.
 Future<String> task2() async {
   Duration duration = Duration(seconds: 3);
 
