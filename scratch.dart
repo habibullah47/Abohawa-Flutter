59cbd9cfb88b52a9af7ce8f8b5f321244cfe2486
habibullah47 <habib.world.121@gmail.com>
2024-09-21 16:05:47 +0600

    await & async function

---
+++ scratch.dart
@@ -0,0 +1,29 @@
+void main() {
+  allTaskCompleted();
+}
+
+void allTaskCompleted() async {
+  task1();
+  String taskTwoResult = await task2();
+  task3(taskTwoResult);
+}
+
+void task1() {
+  print('Task 1 completed');
+}
+
+Future<String> task2() async {
+  Duration duration = Duration(seconds: 3);
+
+  String result = '';
+
+  await Future.delayed(duration, () {
+    result = 'Task 2 data';
+    print('Task 2 completed');
+  });
+  return result;
+}
+
+void task3(String taskTwoData) {
+  print('Task 3 completed with $taskTwoData');
+}
